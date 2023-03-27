76c227f83b748add9a260748e21d682755050a75
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2023-03-27 20:48:20 +0300

    Getting started wit eludo

---
+++ test_eludo_1.ino
@@ -0,0 +1,21 @@
+#include <FastLED.h>
+
+#define LED_PIN     7
+#define NUM_LEDS    30
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+
+CRGB leds[NUM_LEDS];
+
+int leds_cnt[NUM_LEDS];
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
